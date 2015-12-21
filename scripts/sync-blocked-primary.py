9bb8aa282d9d2b063b561374f1202660beac85a3
Peter Robinson <pbrobinson@gmail.com>
2015-12-21 16:00:10 +0000

    Update sync-blocked-primary.py with currrent active releases
    Signed-off-by: Peter Robinson <pbrobinson@fedoraproject.org>

--- scripts/sync-blocked-primary.py
+++ scripts/sync-blocked-primary.py
@@ -17,7 +17,7 @@ import shutil
 
 # Set some variables
 # Some of these could arguably be passed in as args.
-tags = ['f23', 'f22', 'f21', 'f20'] # tag to check in koji
+tags = ['f24', 'f23', 'f22'] # tag to check in koji
 
 arches = ['arm', 'ppc', 's390']
 
