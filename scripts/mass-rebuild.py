87eb74ee3c72b1639ae16f3ea18caeca0e1d8bd0
Peter Robinson <pbrobinson@gmail.com>
2016-02-04 11:08:52 +0000

    Add linux-firmware to exclude list with the other boot stuff
    Signed-off-by: Peter Robinson <pbrobinson@fedoraproject.org>

--- scripts/mass-rebuild.py
+++ scripts/mass-rebuild.py
@@ -26,7 +26,7 @@ workdir = os.path.expanduser('~/massbuild')
 enviro = os.environ
 target = 'f24-rebuild'
 
-pkg_skip_list = ['shim', 'shim-signed', 'kernel', 'grub2', 'openh264']
+pkg_skip_list = ['shim', 'shim-signed', 'kernel', 'linux-firmware', 'grub2', 'openh264']
 
 # Define functions
 
