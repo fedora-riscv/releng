07b8a14d4e0a8c00da5088add529e635c36eaa4c
Dennis Gilmore <dennis@ausil.us>
2016-03-01 11:36:25 -0600

    setup for f24 branching
    Signed-off-by: Dennis Gilmore <dennis@ausil.us>

--- scripts/block_retired.py
+++ scripts/block_retired.py
@@ -17,8 +17,8 @@ from autosigner import SubjectSMTPHandler
 
 
 log = logging.getLogger(__name__)
-RETIRING_BRANCHES = ["el5", "el6", "epel7", "master"]
-PROD_ONLY_BRANCHES = ["el5", "el6", "epel7", "master"]
+RETIRING_BRANCHES = ["el5", "el6", "epel7", "f24", "master"]
+PROD_ONLY_BRANCHES = ["el5", "el6", "epel7", "f24", "master"]
 
 PRODUCTION_PKGDB = "https://admin.fedoraproject.org/pkgdb"
 STAGING_PKGDB = "https://admin.stg.fedoraproject.org/pkgdb"
@@ -41,7 +41,8 @@ class ReleaseMapper(object):
 
         # git branchname, koji tag, epel build tag
         self.mapping = (
-            ("master", "f24", ""),
+            ("master", "f25", ""),
+            ("f24", "f24", ""),
             ("f23", "f23", ""),
             ("f22", "f22", ""),
             ("f21", "f21", ""),
