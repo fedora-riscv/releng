a0ac1f50cc5ab9a3b4759567bf9c6eaa5bc847ae
Mohan Boddu <mboddu@redhat.com>
2016-08-26 14:00:14 -0500

    Remove unnecessary white spaces
    Signed-off-by: Mohan Boddu <mboddu@redhat.com>

--- scripts/stage-release.sh
+++ scripts/stage-release.sh
@@ -30,7 +30,7 @@ fi
 
 BASE="/mnt/koji/compose/"
 
-for checksum in $(find $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/ -name  *CHECKSUM) 
+for checksum in $(find $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/ -name  *CHECKSUM)
 do 
   cat $checksum >/tmp/sum && NSS_HASH_ALG_SUPPORT=+MD5 sigul sign-text -o /tmp/signed $KEY /tmp/sum && chmod 644 /tmp/signed && sudo mv /tmp/signed $checksum
 done
