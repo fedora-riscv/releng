ab56428be413cb23d6be2e2de17647f72062013c
Mohan Boddu <mohanboddu@bodhi-backend01.phx2.fedoraproject.org>
2016-08-26 18:44:45 +0000

    Fix for RELEASEVER variable, add SHORTRELEASEVER where ever necessary, Remove password fix
    Signed-off-by: Mohan Boddu <mohanboddu@bodhi-backend01.phx2.fedoraproject.org>

--- scripts/stage-release.sh
+++ scripts/stage-release.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier:  GPL-2.0+
 
 
-VELEASEVER=$1
+RELEASEVER=$1
 COMPOSEID=$2
 STAGE=$3
 KEY=$4
@@ -20,20 +20,19 @@ ALTDESTDIR="/pub/fedora-secondary/releases/"
 STAGEDIR="/pub/alt/stage/"
 }
 
-if $PRERELEASE == 1
-then
+SHORTRELEASEVER=$(echo $RELEASEVER | sed -e 's|_.*||g')
+
+if [ $PRERELEASE == 1 ]; then
   RELPREFIX="test/"
 else
   RELPREFIX=""
+fi
 
 BASE="/mnt/koji/compose/"
 
-echo "Enter the password for the releasekey"
-read pass
-
-for checksum in $(find $BASE/$RELEASEVER/$COMPOSEID/compose/ -name  *CHECKSUM); 
+for checksum in $(find $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/ -name  *CHECKSUM) 
 do 
-  cat $checksum >/tmp/sum && NSS_HASH_ALG_SUPPORT=+MD5 sigul --batch sign-text -o /tmp/signed $KEY /tmp/sum $pass && chmod 644 /tmp/signed && sudo mv /tmp/signed $checksum
+  cat $checksum >/tmp/sum && NSS_HASH_ALG_SUPPORT=+MD5 sigul sign-text -o /tmp/signed $KEY /tmp/sum && chmod 644 /tmp/signed && sudo mv /tmp/signed $checksum
 done
 
 sudo -u ftpsync mkdir -p $DESTDIR/$RELPREFIX$RELEASEVER/
@@ -43,10 +42,11 @@ sudo -u ftpsync chmod 700 $ALTDESTDIR/$RELPREFIX$RELEASEVER
 
 for dir in Everything CloudImages  Docker Server  Spins  Workstation
 do 
-  sudo -u ftpsync rsync -avhH $BASE/$RELEASEVER/$COMPOSEID/compose/$dir/ $DESTDIR/$RELPREFIX$RELEASEVER/$dir/ --link-dest=/pub/fedora/linux/development/$RELEASEVER/Everything/ --link-dest=$STAGEDIR/$STAGE/Everything/ --link-dest=$STAGEDIR/$STAGE/$dir/
+  sudo -u ftpsync rsync -avhH $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/$dir/ $DESTDIR/$RELPREFIX$RELEASEVER/$dir/ --link-dest=/pub/fedora/linux/development/$SHORTRELEASEVER/Everything/ --link-dest=$STAGEDIR/$STAGE/Everything/ --link-dest=$STAGEDIR/$STAGE/$dir/
 done
 
-for dir in Cloud Labs ; do sudo -u ftpsync rsync -avhH $BASE/$RELEASEVER/$COMPOSEID/compose/$dir/ $ALTDESTDIR/$RELPREFIX$RELEASEVER/$dir/ --link-dest=/pub/fedora/linux/development/$RELEASEVER/Everything/ --link-dest=$STAGEDIR/$STAGE/Everything/ --link-dest=$STAGEDIR/$STAGE/$dir/; done
+for dir in Cloud Labs ; do sudo -u ftpsync rsync -avhH $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/$dir/ $ALTDESTDIR/$RELPREFIX$RELEASEVER/$dir/ --link-dest=/pub/fedora/linux/development/$SHORTRELEASEVER/Everything/ --link-dest=$STAGEDIR/$STAGE/Everything/ --link-dest=$STAGEDIR/$STAGE/$dir/
+done
 
 sudo -u ftpsync scripts/build_composeinfo $DESTDIR/$RELPREFIX$RELEASEVER/
 sudo -u ftpsync scripts/build_composeinfo $ALTDESTDIR/$RELPREFIX$RELEASEVER/
