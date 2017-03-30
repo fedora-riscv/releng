01d40cf6724f88675fc83d9941c41bb27b06539b
Mohan Boddu <mboddu@redhat.com>
2017-03-30 15:44:05 -0400

    Rsync changes to support primary and alt arches
    Now, we have all arches in one compose. So, we need to split
    the compose based on arches and rync them to different locations
    depending upon whether the arch is a primary or an alt arch.
    
    Primary arches : x86_64, armhfp
    Alt arches : aarch64, i386, ppc64, ppc64le
    
    Signed-off-by: Mohan Boddu <mboddu@redhat.com>

--- scripts/stage-release.sh
+++ scripts/stage-release.sh
@@ -8,17 +8,12 @@ COMPOSEID=$2
 STAGE=$3
 KEY=$4
 PRERELEASE=$5
-ARCH=$6
 
 DESTDIR="/pub/fedora/linux/releases/"
 ALTDESTDIR="/pub/alt/releases/"
 STAGEDIR="/pub/alt/stage/"
 
-[ -n "$ARCH" ] && {
-DESTDIR="/pub/fedora-secondary/releases/"
-ALTDESTDIR="/pub/fedora-secondary/releases/"
-STAGEDIR="/pub/alt/stage/"
-}
+ALTARCHDESTDIR="/pub/fedora-secondary/releases/"
 
 SHORTRELEASEVER=$(echo $RELEASEVER | sed -e 's|_.*||g')
 
@@ -39,22 +34,35 @@ sudo -u ftpsync mkdir -p $DESTDIR/$RELPREFIX$RELEASEVER/
 sudo -u ftpsync chmod 700 $DESTDIR/$RELPREFIX$RELEASEVER
 sudo -u ftpsync mkdir -p $ALTDESTDIR/$RELPREFIX$RELEASEVER/
 sudo -u ftpsync chmod 700 $ALTDESTDIR/$RELPREFIX$RELEASEVER
+sudo -u ftpsync mkdir -p $ALTARCHDESTDIR/$RELPREFIX$RELEASEVER/
+sudo -u ftpsync chmod 700 $ALTARCHDESTDIR/$RELPREFIX$RELEASEVER
 
-for dir in Everything CloudImages  Docker Server  Spins  Workstation
-do 
-  sudo -u ftpsync rsync -avhH $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/$dir/ $DESTDIR/$RELPREFIX$RELEASEVER/$dir/ --link-dest=/pub/fedora/linux/development/$SHORTRELEASEVER/Everything/ --link-dest=$STAGEDIR/$STAGE/Everything/ --link-dest=$STAGEDIR/$STAGE/$dir/
-done
+sudo -u ftpsync compose-partial-copy --arch=armhfp --arch=x86_64 --arch src \
+     $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/$dir/ $DESTDIR/$RELPREFIX$RELEASEVER/$dir/ \
+     --variant Everything --variant CloudImages --variant Docker --variant Server --variant Spins --variant Workstation \
+     --link-dest=/pub/fedora/linux/development/$SHORTRELEASEVER/Everything/ --link-dest=$STAGEDIR/$STAGE/Everything/ --link-dest=$STAGEDIR/$STAGE/$dir/
 
-for dir in Cloud Labs ; do sudo -u ftpsync rsync -avhH $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/$dir/ $ALTDESTDIR/$RELPREFIX$RELEASEVER/$dir/ --link-dest=/pub/fedora/linux/development/$SHORTRELEASEVER/Everything/ --link-dest=$STAGEDIR/$STAGE/Everything/ --link-dest=$STAGEDIR/$STAGE/$dir/
-done
+sudo -u ftpsync compose-partial-copy --arch=armhfp --arch=x86_64 --arch src \
+     $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/$dir/ $ALTDESTDIR/$RELPREFIX$RELEASEVER/$dir/ \
+     --variant Cloud --variant Labs \
+     --link-dest=/pub/fedora/linux/development/$SHORTRELEASEVER/Everything/ --link-dest=$STAGEDIR/$STAGE/Everything/ --link-dest=$STAGEDIR/$STAGE/$dir/
+
+
+sudo -u ftpsync compose-partial-copy --arch=aarch64 --arch=i386 --arch=ppc64 --arch=ppc64le \
+     $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/$dir /$ALTARCHDESTDIR/$RELPREFIX$RELEASEVER/$dir/ \
+     --exclude=s390x \
+     --variant Everything --variant Cloud --variant CloudImages --variant Docker --variant Labs --variant Server --variant Spins --variant Workstation \
+     --link-dest=/pub/fedora/linux/development/$SHORTRELEASEVER/Everything/ --link-dest=$STAGEDIR/$STAGE/Everything/ --link-dest=$STAGEDIR/$STAGE/$dir/
 
 sudo -u ftpsync scripts/build_composeinfo $DESTDIR/$RELPREFIX$RELEASEVER/
 sudo -u ftpsync scripts/build_composeinfo $ALTDESTDIR/$RELPREFIX$RELEASEVER/
+sudo -u ftpsync scripts/build_composeinfo $ALTARCHALTDESTDIR/$RELPREFIX$RELEASEVER/
 sudo scripts/build_composeinfo $BASE$RELEASEVER/$COMPOSEID/compose/
 
 sudo -u ftpsync chmod 750 $DESTDIR/$RELPREFIX$RELEASEVER/
 sudo -u ftpsync chmod 750 $ALTDESTDIR/$RELPREFIX$RELEASEVER/
+sudo -u ftpsync chmod 750 $ALTARCHALTDESTDIR/$RELPREFIX$RELEASEVER/
 
 sudo -u ftpsync du -hs $DESTDIR/$RELPREFIX$RELEASEVER/
 sudo -u ftpsync du -hs $ALTDESTDIR/$RELPREFIX$RELEASEVER/
-
+sudo -u ftpsync du -hs $ALTARCHALTDESTDIR/$RELPREFIX$RELEASEVER/
