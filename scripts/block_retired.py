4bb9fbbf4a75a969e4831f5d94c2ff25598d9bab
Till Maas <opensource@till.name>
2017-11-03 21:46:34 +0100

    Do not block packages in f27
    Fedora 27 is in Final Freeze, therefore packages should not be blocked
    there anymore.
    
    Signed-off-by: Till Maas <opensource@till.name>

--- scripts/block_retired.py
+++ scripts/block_retired.py
@@ -14,8 +14,8 @@ import requests
 
 
 log = logging.getLogger(__name__)
-RETIRING_BRANCHES = ["el6", "epel7", "f27", "master"]
-PROD_ONLY_BRANCHES = ["el6", "epel7", "f27", "master"]
+RETIRING_BRANCHES = ["el6", "epel7", "master"]
+PROD_ONLY_BRANCHES = ["el6", "epel7", "master"]
 
 PRODUCTION_PKGDB = "https://admin.fedoraproject.org/pkgdb"
 STAGING_PKGDB = "https://admin.stg.fedoraproject.org/pkgdb"
