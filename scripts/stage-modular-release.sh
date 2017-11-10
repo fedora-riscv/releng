5aacc2248fbc67fbb97577fbac942f43361e924b
Mohan Boddu <mboddu@redhat.com>
2017-11-10 09:53:07 -0500

    Fix staging script for modular composes
    Signed-off-by: Mohan Boddu <mboddu@redhat.com>

--- scripts/stage-modular-release.sh
+++ scripts/stage-modular-release.sh
@@ -7,26 +7,40 @@ RELEASEVER=$1
 COMPOSEID=$2
 STAGE=$3
 KEY=$4
+PRERELEASE=$5
 
-DESTDIR="/pub/alt/unofficial/releases/"
+DESTDIR="/pub/fedora/linux/modular/releases/"
 
 SHORTRELEASEVER=$(echo $RELEASEVER | sed -e 's|_.*||g')
+
+if [ $PRERELEASE == 1 ]; then
+  RELPREFIX="test/"
+else
+  RELPREFIX=""
+fi
 
 BASE="/mnt/koji/compose/"
 
 for checksum in $(find $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/ -name  *CHECKSUM)
-do 
-  cat $checksum >/tmp/sum && NSS_HASH_ALG_SUPPORT=+MD5 sigul sign-text -o /tmp/signed $KEY /tmp/sum && chmod 644 /tmp/signed && sudo mv /tmp/signed $checksum
+do
+  if grep -q BEGIN $checksum; then
+    echo "$checksum is already signed"
+  else	
+    cat $checksum >/tmp/sum && NSS_HASH_ALG_SUPPORT=+MD5 sigul sign-text -o /tmp/signed $KEY /tmp/sum && chmod 644 /tmp/signed && sudo mv /tmp/signed $checksum
+  fi
 done
 
-sudo -u ftpsync mkdir -p $DESTDIR/$RELEASEVER/
-sudo -u ftpsync chmod 700 $DESTDIR/$RELEASEVER
+sudo -u ftpsync mkdir -p $DESTDIR/$RELPREFIX$RELEASEVER/
+sudo -u ftpsync chmod 700 $DESTDIR/$RELPREFIX$RELEASEVER
 
 sudo -u ftpsync compose-partial-copy --arch=armhfp --arch=x86_64 --arch src --arch=aarch64 --arch=i386 --arch=ppc64 --arch=ppc64le \
-     $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/$dir/ $DESTDIR/$RELEASEVER/$dir/ \
-     --variant Server 
+     $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/$dir/ $DESTDIR/$RELPREFIX$RELEASEVER/$dir/ \
+     --variant Server
+
+sudo -u ftpsync scripts/build_composeinfo $DESTDIR/$RELPREFIX$RELEASEVER/
+sudo scripts/build_composeinfo $BASE/$SHORTRELEASEVER/$COMPOSEID/compose/
 
 
-sudo -u ftpsync chmod 755 $DESTDIR/$RELEASEVER/
+sudo -u ftpsync chmod 750 $DESTDIR/$RELEASEVER/
 
 sudo -u ftpsync du -hs $DESTDIR/$RELEASEVER/
