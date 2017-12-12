3c9a4ae31e39a927e336a6afed7b3dc69941a9fd
Mohan Boddu <mboddu@redhat.com>
2017-12-12 10:32:24 -0500

    Adding 25 primary and secondary keys for koji cleanup for 25 EOL
    Signed-off-by: Mohan Boddu <mboddu@redhat.com>

--- scripts/koji_cleanup_signed.py
+++ scripts/koji_cleanup_signed.py
@@ -24,7 +24,8 @@ KEYS = [
     '95a43f54', 'a0a7badb',
     '8e1431d5', 'a29cb19c',
     '873529b8', '34ec9cba',
-    '030d5aed', '81b46521'
+    '030d5aed', '81b46521',
+    'e372e838', 'fdb19c98'
 ]
 
 PREFIX = 'data/signed'
