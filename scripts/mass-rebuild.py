b72bf710d0315873eebba8a2d5fabc035349175f
Peter Robinson <pbrobinson@gmail.com>
2018-03-06 15:52:45 +0000

    add the other shim packages to exclude lists, they need special treatment
    Signed-off-by: Peter Robinson <pbrobinson@gmail.com>

--- scripts/mass-rebuild.py
+++ scripts/mass-rebuild.py
@@ -27,7 +27,7 @@ workdir = os.path.expanduser('~/massbuild')
 enviro = os.environ
 target = 'f28-rebuild'
 
-pkg_skip_list = ['fedora-release', 'fedora-repos', 'generic-release', 'redhat-rpm-config', 'shim', 'shim-signed', 'kernel', 'linux-firmware', 'grub2', 'openh264']
+pkg_skip_list = ['fedora-release', 'fedora-repos', 'generic-release', 'redhat-rpm-config', 'shim', 'shim-signed', 'shim-unsigned-aarch64', 'shim-unsigned-x64', 'kernel', 'linux-firmware', 'grub2', 'openh264']
 
 # Define functions
 
