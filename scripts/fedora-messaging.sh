791a2f7198d28a1221b4af67625c6067da3fddf5
Karsten Hopp <karsten@redhat.com>
2019-10-28 11:03:15 +0100

    revert changes to fedmsg-functions.sh, move fedora-messaging code to new file
    Signed-off-by: Karsten Hopp <karsten@redhat.com>

--- scripts/fedora-messaging.sh
+++ scripts/fedora-messaging.sh
@@ -1,13 +1,14 @@
-# fedmsg-functions
+# fedora-messaging.sh
 #
 # This is a bash shell script that is meant to be sourced by other scripts
-# and aims to deliver functions common to sending messages to fedmsg[0]
-# in rel-eng shell scripts.
+# and aims to deliver functions common to sending messages to 
+# fedora-messaging[0] in rel-eng shell scripts.
 #
-# [0] - http://www.fedmsg.com/en/latest/
+# [0] - https://github.com/fedora-infra/fedora-messaging
 
 
 # NOTES:
+#  FIXME: This might be obsolete:
 #   - Scripts that source this should define at least the following:
 #       FEDMSG_MODNAME
 #       FEDMSG_CERTPREFIX
@@ -16,18 +17,21 @@
 #
 #           FEDMSG_MODNAME="compose"
 #           FEDMSG_CERTPREFIX="bodhi"
-#           source ./scripts/fedmsg-functions.sh
+#           source ./scripts/fedora-messaging.sh
 #
 #           fedmsg_json_start=$(printf '{"log": "start", "branch": "f24", "arch": "x86_64"}')
-#           send_fedmsg "${fedmsg_json_start}" f24 start
+#           send_fedora_message "${fedmsg_json_start}" f24 start
 
-function send_fedmsg()
+# This uses the new fedora-messaging bus:
+LOGGER=./fedora-messaging-logger
+
+function send_fedora_message()
 {
     jsoninput="${1}"
     dist="${2}"
     topic="${3}"
 
-    echo ${jsoninput} | fedmsg-logger \
+    echo ${jsoninput} | $LOGGER \
         --cert-prefix ${FEDMSG_CERTPREFIX} \
         --modname ${FEDMSG_MODNAME} \
         --topic "${dist}.${topic}" \
