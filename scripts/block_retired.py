a9331ccc936bdb06f787556f3f3e0429ab67e5c9
Mohan Boddu <mboddu@bhujji.com>
2021-08-09 14:49:56 -0400

    F34 is out, no more blocking on f34 tag
    Signed-off-by: Mohan Boddu <mboddu@bhujji.com>

--- scripts/block_retired.py
+++ scripts/block_retired.py
@@ -13,8 +13,8 @@ import requests
 
 
 log = logging.getLogger(__name__)
-RETIRING_BRANCHES = ["el6", "epel7", "epel8", "epel8-playground", "rawhide", "f34"]
-PROD_ONLY_BRANCHES = ["el6", "epel7", "epel8", "epel8-playground", "rawhide", "f34"]
+RETIRING_BRANCHES = ["el6", "epel7", "epel8", "epel8-playground", "rawhide"]
+PROD_ONLY_BRANCHES = ["el6", "epel7", "epel8", "epel8-playground", "rawhide"]
 
 PRODUCTION_PDC = "https://pdc.fedoraproject.org"
 STAGING_PDC = "https://pdc.stg.fedoraproject.org"
