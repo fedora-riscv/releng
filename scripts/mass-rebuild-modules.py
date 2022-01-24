1983ecbf33dfabf6c9c97ed7c73471a0365d1468
Tomas Hrcka <thrcka@redhat.com>
2022-01-24 13:33:27 +0100

    F36 Mass rebuild of modules
    Signed-off-by: Tomas Hrcka <thrcka@redhat.com>

--- scripts/mass-rebuild-modules.py
+++ scripts/mass-rebuild-modules.py
@@ -21,7 +21,7 @@ from gi.repository import Modulemd
 
 from massrebuildsinfo import MASSREBUILDS
 
-rebuildid = 'f34'
+rebuildid = 'f36'
 massrebuild = MASSREBUILDS[rebuildid]
 user = 'Fedora Release Engineering <releng@fedoraproject.org>'
 comment = '- Rebuilt for ' + massrebuild['wikipage']
