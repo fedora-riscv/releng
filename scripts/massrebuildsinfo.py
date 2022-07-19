0cca57cc9c3e32cb9613ed315938eb4b2bf86593
Tomas Hrcka <thrcka@redhat.com>
2022-07-19 09:31:27 +0200

    F37 mass rebuild changes
    Signed-off-by: Tomas Hrcka <thrcka@redhat.com>

--- scripts/massrebuildsinfo.py
+++ scripts/massrebuildsinfo.py
@@ -23,9 +23,28 @@ PKG_SKIP_LIST = [
 
 # keep this sorted new -> old
 MASSREBUILDS = {
-    #f37 ftbfs bug tracker: TBA
+    #f38 ftbfs bug tracker: TBA
     "f37":{
-            "tracking_bug": "2045102"
+            "buildtag": 'f37-rebuild',  # tag to build from
+            "epoch": '2022-07-20 17:30:00.000000',  # rebuild anything not built after this date
+            "module_mass_rebuild_epoch": '2022-07-20T22:30:00Z',
+            # rebuild anything not built after this date for modules
+            "module_mass_branching_epoch": '2022-02-08T22:30:00Z',
+            # rebuild anything not built after this date for modules
+            "module_mass_rebuild_platform": "f37",
+            # rebuild all modules that has build time dependency on this platform, this is used during mass rebuild time
+            "module_mass_branching_platform": "f37",
+            # rebuild all modules that has run time dependency on this platform, this is used during mass branching time
+            "targets": ['f37-candidate', 'rawhide', 'f37'],
+            # build targets to check for existing builds to skip rebuild
+            "target": 'f37-rebuild',  # target to build into
+            "desttag": 'f37',  # Tag where fixed builds go
+            "product": "Fedora",  # for BZ product field
+            "version": "rawhide",  # for BZ version field, rawhide before branching or xx after branching
+            "rawhide_version": "37",  # for next version calculation and other comments
+            "tracking_bug": "2045102",  # Tracking bug for mass build failures
+            "wikipage": "https://fedoraproject.org/wiki/Fedora_37_Mass_Rebuild",
+            "pkg_skip_list": PKG_SKIP_LIST,  # packages to skip in rebuild
     },
     "f36":
         {
