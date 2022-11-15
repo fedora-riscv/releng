1012ca29c2e9e764f393a79a47cd1e3d8563b911
Clement Verna <cverna@tutanota.com>
2022-11-15 16:49:12 +0100

    Fedora 37 is now the latest for containers
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- scripts/sync-latest-container-base-image.sh
+++ scripts/sync-latest-container-base-image.sh
@@ -39,7 +39,7 @@ else
 fi
 # This is the release of Fedora that is currently stable, it will define if we
 # need to move the fedora:latest tag
-current_stable="36"
+current_stable="37"
 # Define what is rawhide so we know to push that tag
 current_rawhide="38"
 # Sanity checking
