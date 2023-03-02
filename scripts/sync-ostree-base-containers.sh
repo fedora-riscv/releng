19de81edd22270aeb7ddcd062f4c648008694997
Colin Walters <walters@verbum.org>
2023-03-02 17:25:28 -0500

    sync-ostree-base-containers: Handle rawhide
    xref https://pagure.io/releng/pull-request/11180#comment-184431
    
    Basically we need to do the same mapping from `39` to `rawhide`
    for the ostree refs.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- scripts/sync-ostree-base-containers.sh
+++ scripts/sync-ostree-base-containers.sh
@@ -22,11 +22,21 @@ current_rawhide="39"
 
 ostree_base_images=(silverblue kinoite)
 
+# An optional additional alias for the image; this is only set for current stable and rawhide.
+# For example, given current_stable=36, then the previous release of 35 will just appear as a single
+# fedora-silverblue:35 image, whereas 36 will appear as fedora-silverblue:36 *and* fedora-silverblue:latest.
+# Rawhide is also a special case.
+tagname=
 if [[ ${fedora_version} -eq "$current_stable" ]]; then
     tagname="latest"
 fi
 if [[ ${fedora_version} -eq "$current_rawhide" ]]; then
     tagname="rawhide"
+    # Confusingly, the ostree repository seems not to have an alias for 39, and wants "rawhide" today,
+    # so we are effectively backreferencing that.  This may be a releng bug.
+    ostree_version_component="rawhide"
+else
+    ostree_version_component="${fedora_version}"
 fi
 if [[ -z "${REGISTRY_STAGE:-}" ]]; then
     registries=("registry.fedoraproject.org" "candidate-registry.fedoraproject.org" "quay.io/fedora")
@@ -49,9 +59,19 @@ arch_to_goarch() {
 
 for name in "${ostree_base_images[@]}"; do
     echo "Processing: ${name}"
+    # The base name component, e.g. "fedora-silverblue"
     imgname=fedora-"${name}"
+    # This will be e.g. fedora-silverblue:37 - it always uses a numbered version
     imgtag=${imgname}:${fedora_version}
+    # This is the full expanded canonical image, e.g. quay.io/fedora/fedora-silverblue:37
     primary_imgtag=${canonical_registry}/${imgtag}
+    # aliased_name is e.g. fedora-silverblue:latest instead of fedora-silverblue:37 (if 37 is current stable)
+    aliased_name=
+    # This is only non-empty if tagname is set.
+    if test -n "${tagname}"; then
+        aliased_name=${imgname}:${tagname}
+    fi
+
     work_dir=$(mktemp -d -p /var/tmp --suffix=.sync-ostree)
     pushd ${work_dir} &> /dev/null
     # Note that this command will use the current architecture
@@ -77,7 +97,7 @@ gpgkeypath=/etc/pki/rpm-gpg/
 contenturl=mirrorlist=https://ostree.fedoraproject.org/mirrorlist
 EOF
     fi
-    ostree_ref=fedora:fedora/${fedora_version}/${arch}/${name}
+    ostree_ref=fedora:fedora/${ostree_version_component}/${arch}/${name}
     runv ostree --repo=repo pull --commit-metadata-only $ostree_ref
     current_commit=$(ostree --repo=repo rev-parse $ostree_ref)
     echo "current ${ostree_ref}: $current_commit"
@@ -98,7 +118,7 @@ EOF
     # Download the ostree commit for each architecture, and turn them into container images,
     # then push to the registry
     for arch in "${ARCHES[@]}"; do
-        ostree_ref=fedora:fedora/${fedora_version}/${arch}/${name}
+        ostree_ref=fedora:fedora/${ostree_version_component}/${arch}/${name}
         imgtag_arch=oci:${imgtag}-${arch}
         # To optimize local testing, set REPO_CACHE to a path to a repository which has
         # pre-pulled the ostree data.
@@ -108,12 +128,21 @@ EOF
         runv buildah manifest add --arch=$(arch_to_goarch ${arch}) "${imgtag}" ${imgtag_arch}
     done
 
+    # Push to the primary target registry
     target=docker://${primary_imgtag}
     echo "Pushing to ${target}"
     runv buildah manifest push --all "${imgtag}" ${target}
+    # If we have an alias, synchronize that
+    if test -n "${aliased_name}"; then
+        runv skopeo copy docker://${target} docker://${canonical_registry}/${aliased_name}
+    fi
+    # And handle secondary registries
     echo "Copying to secondary registries"
     for registry in "${secondary_registries[@]}"; do
         runv skopeo copy ${target} docker://${registry}/${imgname}
+        if test -n "${aliased_name}"; then
+            runv skopeo copy ${target} docker://${registry}/${aliased_name}
+        fi
     done
     echo "done synchronizing ${name}"
     popd
